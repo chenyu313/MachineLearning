4d2027c6f901d30a90060d14c1a5c731d6630eac
chenyu <cy123202202@163.com>
2022-06-03 09:09:07 +0800

    cy

--- 01线性回归/2单变量的线性回归/2.2_梯度下降.py
+++ 01线性回归/2单变量的线性回归/2.2_梯度下降.py
@@ -1,8 +1,8 @@
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-#需要在现有数据集上，训练线性回归的参数θ
-#这个部分计算J(Ѳ)，X是矩阵
+
+#计算代价函数
+#创建一个以参数θ为特征函数的代价函数
 def computeCost(X,y,theta):
-    inner=np.power(((X*theta.T)-y),2)
+    #X.dot()表示矩阵点乘
+    inner=np.power((X.dot(theta.T)-y),2)
     return np.sum(inner)/(2*len(X))
